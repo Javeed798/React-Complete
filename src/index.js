5618571eb4b9fe2d45389ad9e9bf1555c3f96cc8
D Javeed Sharif <karishmavahida@gmail.com>
2023-05-09 00:04:46 +0530

    Day-1 of React

--- src/index.js
+++ src/index.js
@@ -1,17 +1,71 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+// function Greeting() {
+//   return (
+//     <>
+//       <Message />
+//       <h2>Hello its my first component</h2>
+//     </>
+//   )
+// }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// const Message = () => {
+//   return (
+//     <>
+//       <h1>Hey im a new Message</h1>
+//       <Person />
+//     </>
+//   )
+// }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// const Person = () => {
+//   return (
+//     <>
+//       <p>Good job message</p>
+//     </>
+//   )
+// }
+
+const BookList = () => {
+  return (
+    <section className='bookList'>
+      <Book />
+    </section>
+  )
+}
+
+const Book = () => {
+  return (
+    <article className='book'>
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  )
+}
+
+const Image = () => (
+  <img src='./images/book-1.jpg' alt='Interesting Facts For Curious Minds' />
+)
+const Title = () => {
+  return (
+    <>
+      <h3>Book of Ages</h3>
+    </>
+  )
+}
+
+const Author = () => {
+  return (
+    <>
+      <h4>Jill Lepore</h4>
+    </>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(<BookList />)
+
+export default BookList
